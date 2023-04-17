17cb07ae9dd3f19764d0a5fbdc6e51b243c490d2
DrKillex <lcollaro1994@gmail.com>
2023-04-17 17:25:03 +0200

    problema colonne

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
-class Prodotto{
+class Prodotto
+{
     protected $tipo;
     public $nome;
     public $img;
@@ -16,7 +17,8 @@ class Prodotto{
     }
 };
 
-class Cibo extends Prodotto{
+class Cibo extends Prodotto
+{
     public $peso;
     public $ingredienti;
     public function __construct($_nome, $_img, $_animale, $_prezzo, $_peso, array $_ingredienti)
@@ -28,7 +30,8 @@ class Cibo extends Prodotto{
     }
 };
 
-class Gioco extends Prodotto{
+class Gioco extends Prodotto
+{
     public $caratteristiche;
     public $dimensioni;
     public function __construct($_nome, $_img, $_animale, $_prezzo, $_caratteristiche, $_dimensioni)
@@ -40,7 +43,8 @@ class Gioco extends Prodotto{
     }
 };
 
-class Accessorio  extends Prodotto{
+class Accessorio  extends Prodotto
+{
     public $materiale;
     public $dimensioni;
     public function __construct($_nome, $_img, $_animale, $_prezzo, $_materiale, $_dimensioni)
@@ -52,23 +56,53 @@ class Accessorio  extends Prodotto{
     }
 };
 
-$ciboProva1 = new Cibo('cibo prova','#', 'cane', 11, 22 ,['carne','verdura']);
-$giocoProva1 = new Gioco('gioco prova','#', 'cane', 11, 'rimbalza come un sasso' ,'10x10');
-$accessorioProva1 = new Accessorio('acc prova','#', 'cane', 11, 'legno' ,'10x10');
+$ciboProva1 = new Cibo('cibo prova', 'img/CiboCane.jpg', 'cane', 11, 22, ['carne', 'verdura']);
+$giocoProva1 = new Gioco('gioco prova', 'img/GiocoCane.jpg', 'cane', 11, 'rimbalza come un sasso', '10x10');
+$accessorioProva1 = new Accessorio('acc prova', 'img/AccessorioCane.jpg', 'cane', 11, 'legno', '10x10');
 
-$listaProdotti = [$ciboProva1, $giocoProva1, $accessorioProva1, new cibo('cibo prova 2','#', 'pesce', 11, 22 ,['carne','verdura'])];
-
+$listaProdotti = [$ciboProva1, $giocoProva1, $accessorioProva1, new cibo('cibo prova 2', 'img/CiboPesce.jpg', 'pesce', 11, 22, ['carne', 'verdura'])];
+// var_dump($listaProdotti);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
+
 <body>
-    
+    <header>
+
+    </header>
+    <main>
+        <div class="container">
+            <div class="row">
+                <?php foreach ($listaProdotti as $prodotto) : ?>
+                    <div class="col-4">
+                        <div class="card">
+                            <img src="<?php echo $prodotto->img ?>" class="card-img-top">
+                            <div class="card-body">
+                                <?php foreach ($prodotto as $key => $value) : ?>
+                                    <?php if ($key == 'nome') : ?>
+                                        <h5 class="card-title"><?php echo $value ?></h5>
+
+                                    <?php endif; ?>
+                            </div>
+                        <?php endforeach; ?>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
 </body>
+
 </html>
